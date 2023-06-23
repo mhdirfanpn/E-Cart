0c1d2ddf8aaa3050e9fb0d4749bb1002860c4474
mhdirfanpn <mhdirfanpn@gmail.com>
2023-06-23 16:25:50 +0530

    second commit

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connection = async()=>{
     try {
-      await  mongoose.connect('mongodb://localhost:27017/e-commerce',{useUnifiedTopology:true});
+      await  mongoose.connect('mongodb+srv://mhdirfanpn:NMDCK1yRXkSuK4FY@cluster0.p8wswr7.mongodb.net/passport',{useUnifiedTopology:true});
       console.log(`database connected`);
     } catch (error) {
         console.log(`error in connection - ${error.message}`);
